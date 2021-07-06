9abed80568b9725b1fbc69abc1509bfc07f32fea
Henrique <64926738+xShielder@users.noreply.github.com>
2021-07-06 18:34:16 -0300

    Sistema de Registro de Academia

---
+++ Sistema_Registro_Academia/src/main/java/br/henrique/registroacademia/DadosCorporais.java
@@ -0,0 +1,63 @@
+package br.henrique.registroacademia;
+
+//Nome: Henrique Cesar Nogarini de Carvalho  RA: 2102374
+
+import com.sun.org.apache.xpath.internal.operations.Neg;
+
+public class DadosCorporais {
+
+    private float Peso;
+    private float Altura;
+    private float Gordura;
+    private String Tipodeteste;
+
+    public DadosCorporais() {
+    }
+
+    public float getPeso() {
+        return Peso;
+    }
+
+    public void setPeso(float peso) throws NegativeException {
+        if(peso < 0){
+            throw new NegativeException();
+        }
+        else {
+            Peso = peso;
+        }
+    }
+
+    public float getAltura() {
+        return Altura;
+    }
+
+    public void setAltura(float altura) throws NegativeException {
+        if(altura < 0) {
+            throw new NegativeException();
+        }
+        else {
+            Altura = altura;
+        }
+        }
+
+    public float getGordura() {
+        return Gordura;
+    }
+
+    public void setGordura(float gordura) throws NegativeException {
+        if(gordura < 0) {
+            throw new NegativeException();
+        }
+        else{
+            Gordura = gordura;
+        }
+    }
+
+    public String getTipodeteste() {
+        return Tipodeteste;
+    }
+
+    public void setTipodeteste(String tipodeteste) {
+        Tipodeteste = tipodeteste;
+    }
+}
